181cb2496223e76782951070ca2664d3727a3687
Nerbzzz <briandulkenraad2004@gmail.com>
2025-03-14 00:32:34 +0100

    Add database pool to main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod api;
+mod database;
 
-use crate::api::v1::*;
+use crate::api::v1::routes::*;
+use crate::database::postgres;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv()?;
 
+    let db_pool = postgres::connect();
+
+    
+
+    Ok(())
 }
